1c10b9d44d34f7aec2beedec0db1d8e1d4dc0829
makun <michaelmakun@aliyun.com>
2017-03-26 22:05:13 +0800

    change config/initializers/carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_privider = "fog"
-    config.fog_credentials {
+    config.fog_provider = "fog"
+    config.fog_credentials = {
       provider:             'AWS',
-      aws_acess_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region:                ENV['AWS_REGION']
     }
