0ea760d3db41b4717688589bcd114c35d3bc42d7
makun <michaelmakun@aliyun.com>
2017-03-27 10:59:18 +0800

    change production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-config.action_mailer.default_url_options = { :host => 'https://fathomless-sea-97122.herokuapp.com/'}
+config.action_mailer.default_url_options = { :host => 'https://fathomless-shore-45450.herokuapp.com/'}
 
 config.action_mailer.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
